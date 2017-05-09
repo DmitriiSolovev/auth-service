54fceca3259c448fd41149840f78a7147c1501b1
DmitrySolovyov <interru777@gmail.com>
2017-05-10 02:47:06 +0400

    Fixed order Oauth2 resource filter (spring boot 1.5.x)

--- src/main/java/com/dmsol/cloud/auth/service/config/WebSecurityConfig.java
+++ src/main/java/com/dmsol/cloud/auth/service/config/WebSecurityConfig.java
@@ -2,8 +2,10 @@ package com.dmsol.cloud.auth.service.config;
 
 import com.dmsol.cloud.auth.service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,6 +18,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER) // https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-1.5-Release-Notes#oauth-2-resource-filter
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
